4d957ee0804968f3dbf5cd1356f3f9cf29aa4d5d
BlackJeff <x>
2021-03-02 23:23:41 +0200

    Управление персонажем

---
+++ Project/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    Object player;
+    Object enemy;
+    private void Awake()
+    {
+        player = Resources.Load("Prefabs/Player");
+        enemy = Resources.Load("Prefabs/Enemy");
+    }
+    void Start()
+    {
+        switch (mob)
+        {
+            case MobToSpawn.None:
+                break;
+            case MobToSpawn.Player:
+                Instantiate(player, transform);
+                break;
+            case MobToSpawn.Enemy:
+                Instantiate(enemy, transform);
+                break;
+        }
+    }
+    public enum MobToSpawn
+    {
+        None,
+        Player,
+        Enemy
+    }
+    public MobToSpawn mob;
+}
